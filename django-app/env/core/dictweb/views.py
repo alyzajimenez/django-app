b990ebba8cb6e37277c162c3a50a4d3b89111978
alyzajimenez <alyzamae003@gmail.com>
2024-05-05 14:10:38 +0800

    first-commit

---
+++ django-app/env/core/dictweb/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    template = 'main/index.html'
+    context = {
+        #dictionary
+    }
+    return render(request, template, context)
